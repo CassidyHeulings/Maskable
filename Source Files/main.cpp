a93094535c351b4f8807eb7802691d49cb9c7d89
CassidyHeulings <cassjheuls@gmail.com>
2026-01-29 20:59:03 -0500

    Game loop - handle input

--- Source Files/main.cpp
+++ Source Files/main.cpp
@@ -29,6 +29,91 @@ int main() {
 
     // game loop
     while (window.isOpen()) {
+
+        /*
+         *************
+         Handle input
+         *************
+         */
+
+        // Handle events by polling
+        Event event;
+        while (window.pollEvent(event)) {
+            // close window
+            if (event.type == sf::Event::Closed) window.close();
+
+            // handle one-off key presses
+            if (event.type == Event::KeyPressed) {
+                // **** inventory slots **** //
+                if (state == State::CRAFTING) {
+                    switch (event.key.code) {
+                        case Keyboard::Num1:
+                            // do something
+                            break;
+                        case Keyboard::Num2:
+                            // do something
+                            break;
+                        case Keyboard::Num3:
+                            // do something
+                            break;
+                        case Keyboard::Num4:
+                            // do something
+                            break;
+                        case Keyboard::Num5:
+                            // do something
+                            break;
+                        case Keyboard::Num6:
+                            // do something
+                            break;
+                        default:
+                            break;
+                    } // end level up key code switch
+                }
+                if (state == State::NEW_GAME) {
+                    // prepare new game level
+                    world.width = 2304; // 64 * 36
+                    world.height = 1920; // 64 * 30
+                    world.left = 0;
+                    world.top = 0;
+                    int sizeTile = 64;
+                    player.spawn(world, resolution, sizeTile); // spawn player in center of arena
+                    clock.restart(); // reset clock to avoid frame jump
+                } // end game preparation
+
+                // **** other key codes **** //
+                switch (event.key.code) {
+                    // return key
+                    case Keyboard::Escape:
+                        // handle based on state
+                        switch (state) {
+                        case State::PLAYING: //pause game
+                                state = State::PAUSED;
+                                break;
+                        case State::PAUSED: // unpause game
+                                state = State::PLAYING;
+                                clock.restart(); // reset clock to avoid frame jump
+                                break;
+                        case State::JOURNAL: // start a new game
+                                state = State::PLAYING;
+                                break;
+                        case State::CRAFTING: // close crafting menu
+                                state = State::PLAYING;
+                                break;
+                        default: break;
+                        } // end return state switch
+                        break;
+                    default: break;
+                } // end other key code switch
+            } // end key pressed
+        } // end event polling
+
+        // real-time key presses (movement)
+        if (state == State::PLAYING) {
+            if (Keyboard::isKeyPressed(Keyboard::W)) player.moveUp();
+            if (Keyboard::isKeyPressed(Keyboard::S)) player.moveDown();
+            if (Keyboard::isKeyPressed(Keyboard::A)) player.moveLeft();
+            if (Keyboard::isKeyPressed(Keyboard::D)) player.moveRight();
+        } // end movement while playing
     }
 
     return 0;
